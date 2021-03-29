8eb5274b8b4a40d04db17f57ee553c35bbb2f65b
nick <bnxaxnb@gmail.com>
2021-03-29 23:47:32 +0300

    Change platform to WPF

---
+++ TradeUpHelper/ViewModels/MainWindowVM.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TradeUpHelper.ViewModels
+{
+    class MainWindowVM : ViewModelBase
+    {
+        public ICommand Save
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    t();
+                });
+            }
+        }
+
+        void t() { }
+    }
+}
