3a63e56866554c8cf60126a8b5f776fb38526a4c
Nick <bnxaxnb@gmail.com>
2021-06-14 16:09:42 +0300

    Fix, added main menu window

---
+++ TradeUpHelper/Views/MainMenuWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TradeUpHelper.Controllers;
+using TradeUpHelper.ViewModels;
+
+namespace TradeUpHelper.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для MainMenuWindow.xaml
+    /// </summary>
+    public partial class MainMenuWindow : Window
+    {
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+            this.MouseLeftButtonDown += delegate { this.DragMove(); };
+            DataContext = new MainMenuWindowVM();
+            this.SourceInitialized += new EventHandler(Window1_SourceInitialized);
+        }
+
+        void Window1_SourceInitialized(object sender, EventArgs e)
+        {
+            WindowSizing.WindowInitialized(this);
+        }
+    }
+}
