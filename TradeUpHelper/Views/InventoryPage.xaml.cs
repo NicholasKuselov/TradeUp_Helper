fcfe4a4cc4d7a2d2dc268365d36599d4930b871d
Nick <bnxaxnb@gmail.com>
2021-06-20 18:36:12 +0300

    added inventory page

---
+++ TradeUpHelper/Views/InventoryPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TradeUpHelper.ViewModels;
+
+namespace TradeUpHelper.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для InventoryPage.xaml
+    /// </summary>
+    public partial class InventoryPage : Page
+    {
+        public InventoryPage()
+        {
+            
+            InitializeComponent();
+            this.DataContext = new InventoryPageVM();
+
+        }
+    }
+}
