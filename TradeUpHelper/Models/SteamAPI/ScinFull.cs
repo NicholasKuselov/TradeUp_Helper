3d2e7ee7c708ccc749a381360e398e47b0479549
Nick <bnxaxnb@gmail.com>
2021-06-21 22:47:21 +0300

    fix

---
+++ TradeUpHelper/Models/SteamAPI/ScinFull.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeUpHelper.Models.SteamAPI
+{
+    class ScinFull : Scin
+    {
+        public string name { get; set; }
+        public string market_hash_name { get; set; }
+        public string market_name { get; set; }
+        public ScinDescriptions[] descriptions { get; set; }
+        public ScinDescriptions[] owner_descriptions { get; set; }
+        public ScinActions[] actions { get; set; }
+        public string icon_url { get; set; }
+
+
+    }
+}
