8b4fe876c13e75ddee8f44d43ea83ce87afb46ff
Nick <bnxaxnb@gmail.com>
2021-11-21 13:31:16 +0200

    [Translator] Added translator class for getting text from the localization files by the text key

---
+++ TradeUpHelper/Controllers/Translator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TradeUpHelper.Controllers
+{
+    class Translator
+    {
+        public static string GetTextByKey(string key)
+        {
+            return (string)Application.Current.Resources[key];
+        }
+    }
+}
